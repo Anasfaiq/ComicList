76de1deb089732f81ee61d71e4ba55e50d5946f5
Anasfaiq <176678988+Anasfaiq@users.noreply.github.com>
2026-04-29 13:50:10 +0700

    adding new menu in navbar component

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -239,6 +239,28 @@ const Navbar = ({ session, currentPage, navigate }: NavbarProps) => {
         </svg>
       ),
     },
+    {
+      label: "About",
+      page: "about",
+      icon: (
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width={24}
+          height={24}
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          strokeWidth={1.5}
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        >
+          <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+          <path d="M12 12m-9 0a9 9 0 1 0 18 0a9 9 0 1 0 -18 0" />
+          <path d="M12 8l.01 0" />
+          <path d="M11 12l1 0l0 4l1 0" />
+        </svg>
+      ),
+    },
   ];
 
   const logoutIcon = (
